57e1c053504e5d5dc475c6ae10d0ea39058d585b
so1ve <nn_201312@163.com>
2022-11-05 19:49:10 +0800

    fix: test

--- test/foo.test.ts
+++ test/foo.test.ts
@@ -1,7 +1,7 @@
-// import { describe, expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 
-// describe("foo", () => {
-//   it("should exported", () => {
-//     expect(foo).toBe(1);
-//   });
-// });
+describe("foo", () => {
+  it("should exported", () => {
+    expect(1).toBe(1);
+  });
+});
