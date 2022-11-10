4b1afd4f2ec7a27253e7f2297219f5dc3dc78418
so1ve <nn_201312@163.com>
2022-11-10 22:36:36 +0800

    chore: format

--- packages/clerc/src/helper.ts
+++ packages/clerc/src/helper.ts
@@ -1,8 +1,8 @@
 import type { Clerc } from "./cli";
 import type { Handler, Inspector, Plugin } from "./types";
 
-export const definePlugin = <T extends Clerc, U extends Clerc> (p: Plugin<T, U>) => p;
+export const definePlugin = <T extends Clerc, U extends Clerc>(p: Plugin<T, U>) => p;
 
-export const defineHandler = <C extends Clerc, K extends keyof C["_commands"]> (_cli: C, _key: K, handler: Handler<C["_commands"], K>) => handler;
+export const defineHandler = <C extends Clerc, K extends keyof C["_commands"]>(_cli: C, _key: K, handler: Handler<C["_commands"], K>) => handler;
 
-export const defineInspector = <C extends Clerc> (_cli: C, inspector: Inspector<C["_commands"]>) => inspector;
+export const defineInspector = <C extends Clerc>(_cli: C, inspector: Inspector<C["_commands"]>) => inspector;
