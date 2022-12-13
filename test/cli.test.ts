731e1b80544f6406a3e4fa212a02d26cc2a9b83d
so1ve <nn_201312@163.com>
2022-12-13 16:17:26 +0800

    chore: update deps

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -287,7 +287,7 @@ describe("cli", () => {
   });
   it("should parse shorthand flag", () => {
     Clerc.create()
-      .command("foo", "foo", {})
+      .command("foo", "foo")
       .on("foo", (ctx) => {
         expect(ctx.name).toBe("foo");
         expect(ctx.raw).toMatchInlineSnapshot(`
