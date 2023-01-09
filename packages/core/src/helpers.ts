0dedf462adc12a15bc394c6e04ea70f70f1105fb
so1ve <nn_201312@163.com>
2023-01-09 15:40:46 +0800

    🐛 fix(core): type

--- packages/core/src/helpers.ts
+++ packages/core/src/helpers.ts
@@ -9,5 +9,6 @@ export const defineHandler = <C extends Clerc, K extends keyof C["_commands"]>(_
 export const defineInspector = <C extends Clerc>(_cli: C, inspector: Inspector<C["_commands"]>) => inspector;
 
 export const defineCommand = <N extends string | RootType, O extends CommandOptions<[...P]>, P extends string[]>(command: Command<N, O & CommandOptions<[...P]>>, handler?: HandlerInCommand<
+  // @ts-expect-error Noooooooooooooooooo
   HandlerContext<Record<N, Command<N, O>> & Record<never, never>, N>
 >): CommandWithHandler<N, O & CommandOptions<[...P]>> => ({ ...command, handler });
