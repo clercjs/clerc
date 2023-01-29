c6740cfcd7614d2d9f22d19c1e3d346bd0adc1b1
so1ve <nn_201312@163.com>
2023-01-29 12:09:53 +0800

    🚀 perf(plugin-help): use text-table

--- packages/plugin-help/src/utils.ts
+++ packages/plugin-help/src/utils.ts
@@ -1,30 +1,7 @@
 import getFuncName from "get-func-name";
-import Table from "cli-table3";
+import textTable from "text-table";
 
-export const table = (...items: string[][]) => {
-  const table = new Table({
-    chars: {
-      "top": "",
-      "top-mid": "",
-      "top-left": "",
-      "top-right": "",
-      "bottom": "",
-      "bottom-mid": "",
-      "bottom-left": "",
-      "bottom-right": "",
-      "left": "",
-      "left-mid": "",
-      "mid": "",
-      "mid-mid": "",
-      "right": "",
-      "right-mid": "",
-      "middle": " ",
-    },
-    style: { "padding-left": 0, "padding-right": 0 },
-  });
-  table.push(...items);
-  return table;
-};
+export const table = (...items: string[][]) => textTable(items);
 
 export const splitTable = (...items: string[][]) => {
   return table(...items).toString().split("\n");
