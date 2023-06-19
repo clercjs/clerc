26794b33db6e7996082d218c18729034fba76351
so1ve <i@mk1.io>
2023-06-19 16:42:33 +0800

    style: format

--- packages/core/src/types/utils.ts
+++ packages/core/src/types/utils.ts
@@ -1,6 +1,7 @@
 import type { CamelCase, Dict, Equals } from "@clerc/utils";
 import type { OmitIndexSignature } from "type-fest";
 
+import type { TypeFlag } from "./type-flag";
 import type {
   Command,
   Commands,
@@ -8,7 +9,6 @@ import type {
   GlobalFlagOptions,
   InspectorContext,
 } from ".";
-import type { TypeFlag } from "./type-flag";
 
 type StripBrackets<Parameter extends string> = Parameter extends
   | `<${infer ParameterName}>`
