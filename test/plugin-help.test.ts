b0f6ecf381174c44ed8b56c9521697f4e985fd5b
so1ve <nn_201312@163.com>
2023-07-29 04:12:13 +0800

    test: re-enable tests for plugin-help

--- test/plugin-help.test.ts
+++ test/plugin-help.test.ts
@@ -5,13 +5,14 @@ import { afterEach, beforeAll, describe, expect, it } from "vitest";
 
 import { Cli } from "./create-cli";
 
-describe.skip("plugin-help", () => {
+const NEWLINE_RE = /\r?\n/g;
+
+describe("plugin-help", () => {
 	const msgs: string[] = [];
 
 	beforeAll(() => {
-		// eslint-disable-next-line no-console
-		console.log = (s: string) => {
-			msgs.push(s);
+		(process.stdout.write as any) = (s: string) => {
+			msgs.push(s.replace(NEWLINE_RE, "\n"));
 		};
 	});
 
@@ -23,73 +24,83 @@ describe.skip("plugin-help", () => {
 		Cli().use(helpPlugin()).parse(["help"]);
 
 		expect(msgs).toMatchInlineSnapshot(`
-        [
-          "[1m[90mName:[39m[22m   [90m [39m[31mtest[39m
-        [1m[90mVersion:[39m[22m[90m [39m[33m0.0.0[39m
+			[
+			  "[1mName:[22m     [31mtest[39m
+			[1mVersion:[22m  [33m0.0.0[39m
 
-        [1mDescription:[22m
+			[1mDescription:[22m
 
-            test
+			    test
 
-        [1mUsage:[22m
+			[1mUsage:[22m
 
-            $ test [command] [options]
+			    [35m$ test <command> [flags][39m
 
-        [1mCommands:[22m
+			[1mCommands:[22m
 
-            [36mhelp[39m[90m [39m[33m-[39m[90m [39mShow help
-        ",
-        ]
-      `);
+			    [36mtest help[39m  [33m-[39m  Show help
 
-		msgs.length = 0;
+			[1mGlobal Flags:[22m
+
+			    [34m--help, -h[39m    [33m-[39m  Show help  (Default: false)
+			",
+			]
+		`);
 	});
 
 	it("should show --help", () => {
 		Cli().use(helpPlugin()).parse(["--help"]);
 
 		expect(msgs).toMatchInlineSnapshot(`
-      [
-        "[1m[90mName:[39m[22m   [90m [39m[31mtest[39m
-      [1m[90mVersion:[39m[22m[90m [39m[33m0.0.0[39m
+			[
+			  "[1mName:[22m     [31mtest[39m
+			[1mVersion:[22m  [33m0.0.0[39m
 
-      [1mDescription:[22m
+			[1mDescription:[22m
 
-          test
+			    test
 
-      [1mUsage:[22m
+			[1mUsage:[22m
 
-          $ test [command] [options]
+			    [35m$ test <command> [flags][39m
 
-      [1mCommands:[22m
+			[1mCommands:[22m
 
-          [36mhelp[39m[90m [39m[33m-[39m[90m [39mShow help
-      ",
-      ]
-    `);
+			    [36mtest help[39m  [33m-[39m  Show help
+
+			[1mGlobal Flags:[22m
+
+			    [34m--help, -h[39m    [33m-[39m  Show help  (Default: false)
+			",
+			]
+		`);
 	});
 
 	it("should show name, description and version", () => {
 		Cli().use(helpPlugin()).parse(["help"]);
 
 		expect(msgs).toMatchInlineSnapshot(`
-    [
-      "[1m[90mName:[39m[22m   [90m [39m[31mtest[39m
-    [1m[90mVersion:[39m[22m[90m [39m[33m0.0.0[39m
+			[
+			  "[1mName:[22m     [31mtest[39m
+			[1mVersion:[22m  [33m0.0.0[39m
 
-    [1mDescription:[22m
+			[1mDescription:[22m
 
-        test
+			    test
 
-    [1mUsage:[22m
+			[1mUsage:[22m
 
-        $ test [command] [options]
+			    [35m$ test <command> [flags][39m
 
-    [1mCommands:[22m
+			[1mCommands:[22m
 
-        [36mhelp[39m[90m [39m[33m-[39m[90m [39mShow help
-    ",
-    ]
-  `);
+			    [36mtest help[39m  [33m-[39m  Show help
+
+			[1mGlobal Flags:[22m
+
+			    [34m--help, -h[39m    [33m-[39m  Show help  (Default: false)
+			",
+			]
+		`);
 	});
 });
