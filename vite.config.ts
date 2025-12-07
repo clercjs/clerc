5fdea41d326f2763de654458d2bc06b45ad9fff6
so1ve <i@mk1.io>
2025-12-08 04:29:06 +0800

    use vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,11 @@
-import { resolve } from "node:path";
-
-import type { AliasOptions } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
-const r = (p: string) => resolve(__dirname, p);
-
-export const alias: AliasOptions = {
-	"@clerc/core": r("./packages/core/src"),
-	"@clerc/utils": r("./packages/utils/src"),
-	"@clerc/test-utils": r("./packages/test-utils/src"),
-	"@clerc/parser": r("./packages/parser/src"),
-	"@clerc/plugin-help": r("./packages/plugin-help/src"),
-	"@clerc/plugin-version": r("./packages/plugin-version/src"),
-	"@clerc/plugin-friendly-error": r("./packages/plugin-friendly-error/src"),
-	"@clerc/plugin-not-found": r("./packages/plugin-not-found/src"),
-	"@clerc/plugin-completions": r("./packages/plugin-completions/src"),
-	"@clerc/plugin-strict-flags": r("./packages/plugin-strict-flags/src"),
-};
-
 export default defineConfig({
+	plugins: [tsconfigPaths()],
 	test: {
 		coverage: {
 			provider: "istanbul",
 		},
-	},
-	resolve: {
-		alias,
 	},
 });
