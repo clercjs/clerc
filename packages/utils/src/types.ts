a9a86562bd6d7b8f341d4b26e15801386a0326db
so1ve <i@mk1.io>
2025-12-09 17:41:29 +0800

    fix type

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -10,7 +10,16 @@ export type MaybeArray<T> = T | T[];
 export type Prettify<T> = {
 	[K in keyof T]: T[K];
 } & {};
+export type DeepPrettify<T> = {
+	[K in keyof T]: T[K] extends object ? DeepPrettify<T[K]> : T[K];
+} & {};
 
 export type PartialRequired<T, K extends keyof T> = T & {
 	[P in K]-?: T[P];
 };
+
+export type UnionToIntersection<U> = (
+	U extends any ? (k: U) => void : never
+) extends (k: infer I) => void
+	? I
+	: never;
