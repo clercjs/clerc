76c4350b644622e025885694bef2ca77834ac084
so1ve <i@mk1.io>
2025-12-10 18:03:16 +0800

    fix help test

--- packages/plugin-help/test/plugin-help.test.ts
+++ packages/plugin-help/test/plugin-help.test.ts
@@ -4,30 +4,32 @@ import { afterEach, describe, expect, it, vi } from "vitest";
 import { helpPlugin } from "../src";
 
 describe("plugin-help", () => {
-	const consoleLogSpy = vi.spyOn(console, "log").mockImplementation(() => {});
+	const processStdoutWriteSpy = vi
+		.spyOn(process.stdout, "write")
+		.mockImplementation(() => true);
 
 	afterEach(() => {
-		consoleLogSpy.mockClear();
+		processStdoutWriteSpy.mockClear();
 	});
 
 	it("should show help", () => {
 		Cli().use(helpPlugin()).parse(["help"]);
 
-		expect(consoleLogSpy).toHaveBeenCalled();
-		expect(consoleLogSpy.mock.calls[0][0]).toMatchSnapshot();
+		expect(processStdoutWriteSpy).toHaveBeenCalled();
+		expect(processStdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
 	});
 
 	it("should show --help", () => {
 		Cli().use(helpPlugin()).parse(["--help"]);
 
-		expect(consoleLogSpy).toHaveBeenCalled();
-		expect(consoleLogSpy.mock.calls[0][0]).toMatchSnapshot();
+		expect(processStdoutWriteSpy).toHaveBeenCalled();
+		expect(processStdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
 	});
 
 	it("should show help when no command", () => {
 		Cli().use(helpPlugin()).parse([]);
 
-		expect(consoleLogSpy).toHaveBeenCalled();
-		expect(consoleLogSpy.mock.calls[0][0]).toMatchSnapshot();
+		expect(processStdoutWriteSpy).toHaveBeenCalled();
+		expect(processStdoutWriteSpy.mock.calls[0][0]).toMatchSnapshot();
 	});
 });
