58814a6eb6dd7759313cdb7da74e9ea98e4f2f9e
so1ve <i@mk1.io>
2025-12-11 21:04:50 +0800

    feat(clerc)!: drop extending `Clerc`, export a function `Cli` instead

--- packages/clerc/src/index.ts
+++ packages/clerc/src/index.ts
@@ -1,17 +1,8 @@
-import { Clerc as ClercBase } from "@clerc/core";
+import { Clerc } from "@clerc/core";
 import { helpPlugin } from "@clerc/plugin-help";
 import { versionPlugin } from "@clerc/plugin-version";
 
 export * from "./re-exports";
 
-// @ts-expect-error TS2675
-export class Clerc extends ClercBase {
-	public static create(
-		options?: Parameters<typeof ClercBase.create>[0],
-	): ClercBase {
-		const instance = super.create(options);
-		instance.use(versionPlugin()).use(helpPlugin());
-
-		return instance;
-	}
-}
+export const Cli = (): Clerc =>
+	Clerc.create().use(versionPlugin()).use(helpPlugin());
--- packages/clerc/src/re-exports.ts
+++ packages/clerc/src/re-exports.ts
@@ -1,5 +1,4 @@
 export * from "@clerc/core";
-export { Clerc as ClercBase } from "@clerc/core";
 export * from "@clerc/plugin-completions";
 export * from "@clerc/plugin-friendly-error";
 export * from "@clerc/plugin-help";
