6501597742f99eed8a4d9bba65f22a14956f2d0d
so1ve <i@mk1.io>
2025-12-12 16:05:24 +0800

    chore: use tsdown's workspace functinality to build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
+	workspace: {
+		include: ["packages/*"],
+		exclude: ["packages/test-utils"],
+	},
+	entry: ["src/index.ts"],
+	dts: {
+		oxc: true,
+	},
 	clean: true,
 	fixedExtension: false,
 	external: ["@clerc/core"],
