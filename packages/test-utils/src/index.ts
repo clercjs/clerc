edc8fa290676817d08f1fed970182b98778a7371
so1ve <i@mk1.io>
2025-12-17 17:32:17 +0800

    refactor(test-utils): use `vi.mocked`

--- packages/test-utils/src/index.ts
+++ packages/test-utils/src/index.ts
@@ -1,6 +1,6 @@
 import { Clerc } from "@clerc/core";
 import { Cli } from "clerc";
-import type { MockInstance } from "vitest";
+import { vi } from "vitest";
 
 export const TestBaseCli = () =>
 	Clerc.create().scriptName("test").description("test").version("0.0.0");
@@ -9,4 +9,4 @@ export const TestCli = () =>
 	Cli().scriptName("test").description("test").version("0.0.0");
 
 export const getConsoleMock = (method: keyof Console) =>
-	console[method] as any as MockInstance;
+	vi.mocked(console[method]);
