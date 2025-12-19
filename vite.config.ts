cf9a73976353ba7724968ac866eb94b7e20115cd
so1ve <i@mk1.io>
2025-12-20 05:46:11 +0800

    test: remove `tsconfigPaths` plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
-import tsconfigPaths from "vite-tsconfig-paths";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { defineConfig } from "vitest/config";
 
+import tsconfigBase from "./tsconfig.base.json";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const alias = Object.fromEntries(
+	Object.keys(tsconfigBase.compilerOptions.paths ?? {}).map((key) => {
+		const { paths } = tsconfigBase.compilerOptions;
+		const path = paths[key as keyof typeof paths][0];
+		if (!path) {
+			throw new Error(`Path for ${key} not found`);
+		}
+
+		return [key, join(__dirname, path)];
+	}),
+);
+
 export default defineConfig({
-	plugins: [tsconfigPaths()],
+	resolve: {
+		alias,
+	},
 	test: {
 		coverage: {
 			provider: "istanbul",
