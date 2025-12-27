183f02fbb6298ce494f68f139a659499e3672b24
so1ve <i@mk1.io>
2025-12-28 03:49:16 +0800

    refactor: use asterisk for tsconfig path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,8 @@
-import { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url";
-
+import TsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
-import tsconfigBase from "./tsconfig.base.json";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const alias = Object.fromEntries(
-	Object.keys(tsconfigBase.compilerOptions.paths ?? {}).map((key) => {
-		const { paths } = tsconfigBase.compilerOptions;
-		const path = paths[key as keyof typeof paths][0];
-		if (!path) {
-			throw new Error(`Path for ${key} not found`);
-		}
-
-		return [key, join(__dirname, path)];
-	}),
-);
-
 export default defineConfig({
-	resolve: {
-		alias,
-	},
+	plugins: [TsconfigPaths()],
 	test: {
 		coverage: {
 			provider: "istanbul",
