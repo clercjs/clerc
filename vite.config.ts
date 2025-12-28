332f213b4fa9154f54ccb46d007381065d204b65
so1ve <i@mk1.io>
2025-12-28 16:25:01 +0800

    test: migrate to `vite-plugin-simple-tsconfig-alias`

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import TsconfigPaths from "vite-tsconfig-paths";
+import TsconfigAlias from "vite-plugin-simple-tsconfig-alias";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [TsconfigPaths()],
+  plugins: [
+    TsconfigAlias({
+      configNames: ["tsconfig.base.json"],
+    }),
+  ],
   test: {
     coverage: {
       provider: "istanbul",
